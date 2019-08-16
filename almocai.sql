06bbab686029adb58bfafaf461eee4eafbae38e2
lucas427 <48414967+lucas427@users.noreply.github.com>
2019-08-16 14:11:11 -0300

    +'diaSemana' tabela +'diaSemana_codigo' campo de 'diaAlmoco'

--- almocai.sql
+++ almocai.sql
@@ -1,25 +1,33 @@
 create database almocai;
 use almocai;
 
-create table SemanaCardapio (
+create table if not exists SemanaCardapio (
 	codigo int primary key auto_increment
 );
 
-create table DiaAlmoco (
+create table if not exists DiaSemana (
+	codigo int primary key auto_increment,
+    descricao varchar(45)
+);
+insert into DiaSemana (descricao) values ('Segunda'), ('Terça'), ('Quarta'), ('Quinta'), ('Sexta');
+
+create table if not exists DiaAlmoco (
 	codigo int primary key auto_increment,
     `data` date,
     semanaCardapio_codigo int,
-    foreign key (semanaCardapio_codigo) references SemanaCardapio(codigo)
+    diaSemana_codigo int,
+    foreign key (semanaCardapio_codigo) references SemanaCardapio(codigo),
+    foreign key (diaSemana_codigo) references DiaSemana(codigo)
 );
 
-create table Alimento (
+create table if not exists Alimento (
 	codigo int primary key auto_increment,
     descricao varchar(100),
     diaAlmoco_codigo int,
     foreign key (diaAlmoco_codigo) references DiaAlmoco(codigo)
 );
 
-create table Aluno(
+create table if not exists Aluno (
     matricula int primary key,
     senha varchar(40),
     nome varchar(100)

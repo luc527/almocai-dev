35829004fcf8bcde9078c86fc786c440677054ef
Rian Dziuba <riandziuba@gmail.com>
2019-08-16 10:55:09 -0300

    Re-organizar

--- autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-	spl_autoload_register(function ($nomeClasse) {
-		if (file_exists("classes".DIRECTORY_SEPARATOR.$nomeClasse.".class.php")) {
-			require_once("classes".DIRECTORY_SEPARATOR.$nomeClasse.".class.php");
-		}
-	});
-?>

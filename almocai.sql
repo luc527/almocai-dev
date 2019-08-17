de5dc4297f01b9893e10f26daf6b13e2953b450b
lucas427 <48414967+lucas427@users.noreply.github.com>
2019-08-17 11:55:28 -0300

    trigger que cria 5 dias para uma semana automaticamente após a semana ser criada: segunda, terça ... com as datas correspondentes

--- almocai.sql
+++ almocai.sql
@@ -2,7 +2,8 @@ create database almocai;
 use almocai;
 
 create table if not exists SemanaCardapio (
-	codigo int primary key auto_increment
+	codigo int primary key auto_increment,
+    data_inicio date -- o primeiro dia (segunda) da semana, p. ex. 2019-08-12
 );
 
 create table if not exists DiaSemana (
@@ -18,7 +19,21 @@ create table if not exists DiaAlmoco (
     diaSemana_codigo int,
     foreign key (semanaCardapio_codigo) references SemanaCardapio(codigo),
     foreign key (diaSemana_codigo) references DiaSemana(codigo)
-);
+); 
+
+delimiter :)
+create trigger cria_dias_semana
+after insert on SemanaCardapio 
+for each row
+begin
+	insert into DiaAlmoco (`data`, semanaCardapio_codigo, diaSemana_codigo) values
+    (new.data_inicio, new.codigo, 1),
+    (date_add(new.data_inicio, interval 1 day), new.codigo, 2),
+    (date_add(new.data_inicio, interval 2 day), new.codigo, 3),
+    (date_add(new.data_inicio, interval 3 day), new.codigo, 4),
+    (date_add(new.data_inicio, interval 4 day), new.codigo, 5);
+end :)
+delimiter ;
 
 create table if not exists Alimento (
 	codigo int primary key auto_increment,

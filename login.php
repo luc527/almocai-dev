b50a797eabece27b52aad45a2c91a9c8dd0251aa
lucas427 <48414967+lucas427@users.noreply.github.com>
2019-08-24 12:43:02 -0300

    retorno erros no login ao usuário

--- login.php
+++ login.php
@@ -1,18 +1,36 @@
 <!DOCTYPE html>
+
+<?php
+
+if (isset($_GET['erro'])) $erro = $_GET['erro'];
+else if (isset($_POST['erro'])) $erro = $_POST['erro'];
+else $erro = '';
+
+?>
+
 <html>
 <head>
 	<title>Login</title>
 	<meta charset="utf-8">
 </head>
 <body>
-	<form action="aluno_acao.php" method="post"> <fieldset>
-		<label for="matricula">Matrícula: </label> <br/>
-		<input type="text" name="matricula"> <br/><br/>
+	<div class="container" style="width:70%; margin: auto;">
+		<form action="aluno_acao.php" method="post"> <fieldset>
+			<label for="matricula">Matrícula: </label> <br/>
+			<input type="text" name="matricula"> <br/><br/>
 
-		<label for="senha">Senha: </label> <br/>
-		<input type="password" name="senha"> <br/><br/>
+			<label for="senha">Senha: </label> <br/>
+			<input type="password" name="senha"> <br/><br/>
 
-		</fieldset> <button type="submit" name="acao" value="Login">Login</button>
-	</form>
+			<button type="submit" name="acao" value="Login">Login</button>
+		</fieldset> </form>
+		<br/>
+		<?php
+			if ($erro == 'matricula_nao_existe') {
+				echo "<i>Erro: a matrícula informada não existe.</i>";
+			} else if ($erro == 'senha_incorreta') {
+				echo "<i>Erro: a senha informada está incorreta.</i>";
+			}
+		?>
 </body>
 </html>

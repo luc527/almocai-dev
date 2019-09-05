8c70890f9eef56b279327ad3e9a415b2fae41bdd
lucas427 <lucas.schwambach52@gmail.com>
2019-09-05 13:16:24 -0300

    Os tipos de user, alimento e dia não precisam existir em tabelas separadas por 1:N e é mais fácil assim

--- almocai.sql
+++ almocai.sql
@@ -6,19 +6,13 @@ create table if not exists SemanaCardapio (
     data_inicio date -- o primeiro dia (segunda) da semana, p. ex. 2019-08-12
 );
 
-create table if not exists DiaSemana (
-	codigo int primary key auto_increment,
-	descricao varchar(45)
-);
-
 create table if not exists DiaAlmoco (
 	codigo int primary key auto_increment,
 	`data` date,
 	semanaCardapio_codigo int,
-	diaSemana_codigo int,
+	diaSemana varchar(45),
 
-	foreign key (semanaCardapio_codigo) references SemanaCardapio(codigo),
-	foreign key (diaSemana_codigo) references DiaSemana(codigo)
+	foreign key (semanaCardapio_codigo) references SemanaCardapio(codigo)
 );
 
 delimiter :)
@@ -26,61 +20,34 @@ create trigger cria_dias_semana
 after insert on SemanaCardapio
 for each row
 begin
-	insert into DiaAlmoco (`data`, semanaCardapio_codigo, diaSemana_codigo) values
-    (new.data_inicio, new.codigo, 1),
-    (date_add(new.data_inicio, interval 1 day), new.codigo, 2),
-    (date_add(new.data_inicio, interval 2 day), new.codigo, 3),
-    (date_add(new.data_inicio, interval 3 day), new.codigo, 4),
-    (date_add(new.data_inicio, interval 4 day), new.codigo, 5);
+	insert into DiaAlmoco (`data`, semanaCardapio_codigo, diaSemana) values
+    (new.data_inicio, new.codigo, 'Segunda-feira'),
+    (date_add(new.data_inicio, interval 1 day), new.codigo, 'Terça-feira'),
+    (date_add(new.data_inicio, interval 2 day), new.codigo, 'Quarta-feira'),
+    (date_add(new.data_inicio, interval 3 day), new.codigo, 'Quinta-feira'),
+    (date_add(new.data_inicio, interval 4 day), new.codigo, 'Sexta-feira');
 end :)
 delimiter ;
-
-create table if not exists Tipo_alimento (
-	codigo int primary key auto_increment,
-	descricao varchar(50)
-);
-insert into Tipo_alimento values
-	(1, "Normal"),
-	(2, "Carne"),
-	(3, "Opção vegetariana"),
-	(4, "Opção vegana");
 
 create table if not exists Alimento (
 	codigo int primary key auto_increment,
 	descricao varchar(100),
 	diaAlmoco_codigo int,
-	tipo_cod int,
-
-	foreign key (tipo_cod) references Tipo_alimento(codigo)
-		on delete set null
-		on update cascade,
-
+	tipo varchar(45),
+    
 	foreign key (diaAlmoco_codigo) references DiaAlmoco(codigo)
 		on delete cascade
 		on update cascade
 );
 
-create table if not exists Tipo_usuario (
-	codigo int primary key auto_increment,
-	descricao varchar(50)
-);
-insert into Tipo_usuario (codigo, descricao) values
-	(1, "Administrador(a)"),
-	(2, "Funcionário(a)"),
-	(3, "Aluno(a)");
-
 create table if not exists Usuario (
 	matricula int primary key auto_increment,
 	senha varchar(255),
 	nome varchar(100),
-	tipo_cod int,
-
-	foreign key (tipo_cod) references Tipo_usuario(codigo)
-		on update cascade
-		on delete set null
+	tipo varchar(50)
 );
-insert into Usuario (matricula, senha, nome, tipo_cod) values
-('2019','d033e22ae348aeb5660fc2140aec35850c4da997','admin',1);
+insert into Usuario (matricula, senha, nome, tipo) values
+('2019','d033e22ae348aeb5660fc2140aec35850c4da997','admin','ADM');
 -- senha (provisória): admin
 
 create table if not exists Presenca (

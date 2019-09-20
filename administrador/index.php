399c6f9a88bdd6a2744cb938771753402d3feb7b
lucas427 <48414967+lucas427@users.noreply.github.com>
2019-09-20 13:17:11 -0300

    index Painel do administrador em template + assets novos

---
+++ administrador/index.php
@@ -0,0 +1,34 @@
+<?php
+$root_path = "../";
+
+// Valida seção (inclusive para que só adminsitradores acessem o painel)
+include($root_path."valida_secao.php");
+valida_secao($root_path);
+if ($_SESSION['tipo'] != 'ADMINISTRADOR') {
+  header("location:".$root_path."entrar"); 
+}
+
+// Valores a ser carregados no template {}
+$title = 'Painel do administrador';
+$root_path = $root_path;
+$peso_fonte = ",200";
+
+// Componentes HTML a ser carregados no template {{}}
+$nav = file_get_contents($root_path."componentes/nav-transparent.html");
+$footer = file_get_contents($root_path."componentes/footer.html");
+$scripts = file_get_contents('scripts.js');
+// Componente main
+$main = file_get_contents("main.html");
+
+// Carregamento dos valores e componentes no template
+$painel_adm = file_get_contents($root_path."template.html");
+$painel_adm = str_replace("{title}", $title, $painel_adm);
+$painel_adm = str_replace("{peso_fonte}", $peso_fonte, $painel_adm);
+$painel_adm = str_replace("{{nav}}", $nav, $painel_adm);
+$painel_adm = str_replace("{{footer}}", $footer, $painel_adm);
+$painel_adm = str_replace("{{scripts}}", $scripts, $painel_adm);
+$painel_adm = str_replace("{{main}}", $main, $painel_adm);
+
+$painel_adm = str_replace("{root_path}", $root_path, $painel_adm);
+print($painel_adm);
+?>

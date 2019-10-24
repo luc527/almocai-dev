bb09b7c1a928a035a30c72fd091e6f7418733c9b
riandziuba <riandziuba@gmail.com>
2019-10-24 17:02:35 -0300

    Ajax

--- administrador/componentes/ajax.js
+++ administrador/componentes/ajax.js
@@ -5,21 +5,24 @@ window.onload = function(){
 }
 
 function ValidaUsuario(){
-  ajax = new XMLHttpRequest();
-    ajax.onreadystagechange = function(){
-    if(ajax.status == 200){
-      if(ajax.responseText == 1){
-        document.getElementById('username').classList.add('invalid');
-        document.getElementById('username').classList.remove('valid');
-      }else{
-        document.getElementById('username').classList.remove('invalid');
-        document.getElementById('username').classList.add('valid');
-      }
+  request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200){
+
+        if(request.responseText == 1){
+          document.getElementById('username').classList.add('invalid');
+          document.getElementById('username').classList.remove('valid');
+          document.getElementById("adicionar").disabled = true;
+        }else{
+          document.getElementById('username').classList.remove('invalid');
+          document.getElementById('username').classList.add('valid');
+          document.getElementById("adicionar").disabled = false;
+        }
     }
   }
 
   username = document.getElementById('username').value;
-  ajax.open('GET', '../acao.php?acao=ValidarUsuario&username='+username, true);
-  ajax.send();
+  request.open('GET', '../acao.php?acao=ValidarUsuario&username='+username, true);
+  request.send();
 
 }

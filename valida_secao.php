abd5c5da85b90322ed26286c647080ab46c4b79c
lucas427 <lucas.schwambach52@gmail.com>
2019-10-25 13:00:12 -0300

    Valida seção de múltiplos tipos de usuário

--- valida_secao.php
+++ valida_secao.php
@@ -9,8 +9,19 @@ function valida_secao($root_path) {
 
 function valida_secao_tipo ($root_path, $tipo) {
   valida_secao($root_path);
-  if ($_SESSION['tipo'] != $tipo) {
-    header("location:" . $root_path . "entrar");
+  
+  if (is_array($tipo)) {
+    
+    if (!in_array($_SESSION['tipo'], $tipo)) {
+      header("location:" . $root_path . "entrar");  
+    }
+
+  } else {
+
+    if ($_SESSION['tipo'] != $tipo) {
+      header("location:" . $root_path . "entrar");
+    }
+
   }
 }
 ?>
